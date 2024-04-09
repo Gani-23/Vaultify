4b8d456236ddc91fa05456315be6a8ed8b75884a
gani-23 <Saiganesh7989@gmail.com>
2024-04-09 19:19:18 +0530

    More interactive CSS

--- static/index.js
+++ static/index.js
@@ -1,23 +1,23 @@
 var eventSource = new EventSource('/stream');
-eventSource.onmessage = function(event) {
+eventSource.onmessage = function (event) {
     var data = JSON.parse(event.data);
     var cpuPercent = data.cpu_percent;
     var cpuElement = document.getElementById('cpu_percent');
-    
+
     cpuElement.innerText = 'CPU ' + cpuPercent + '%';
-    
+
     if (cpuPercent >= 80) {
-       alert('Cpu exeeds 80%')
-    
+        alert('Cpu exeeds 80%')
+
     } else if (cpuPercent >= 90) {
         alert('Cpu exeeds 90%')
     }
-    else{
+    else {
         return
     }
 };
 
-setTimeout(function() {
+setTimeout(function () {
     var alertElement = document.querySelector('.alert');
     if (alertElement) {
         alertElement.remove();
